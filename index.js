2fb060986510b9a7564f182fd56290c28ac7a76f
canton32 <>
2021-04-20 00:46:38 -0700

    fix error on getting library path

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const path = require('path')
 const os = require('os')
 
 module.exports = () => {
-  const cwd = path.resolve(__dirname)
+  const cwd = path.resolve(__dirname);
 
   let lib_path;
   const platform = os.platform();
 
+  console.log("__dirname:", path.resolve(__dirname));
+
   if (platform == "darwin")
-    lib_path = `${cwd}/bin/mac/libtdjson.1.7.3.dylib`;
+    lib_path = path.join(cwd, "/bin/mac/libtdjson.1.7.3.dylib");
   else if (platform == "linux")
-    lib_path = `${cwd}/bin/linux/libtdjson.so.1.7.3"`;
+    lib_path = path.join(cwd, "/bin/linux/libtdjson.so.1.7.3");
 
-  if (fs.existsSync(lib_path)) {
-    return lib_path
-  }
+  // if (fs.existsSync(lib_path)) {
+  console.log("return path:", lib_path);
 
-  return null
+  return lib_path
+  // }
+
+  // return null
 }
