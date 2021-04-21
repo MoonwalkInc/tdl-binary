829dab0665b3f90bfeaece2f9e16c23490b46e8f
canton32 <antonverhoeven32@gmail.com>
2021-04-20 18:22:24 -0700

    update binaries v1.6

--- index.js
+++ index.js
@@ -8,18 +8,10 @@ module.exports = () => {
   let lib_path;
   const platform = os.platform();
 
-  console.log("__dirname:", path.resolve(__dirname));
-
   if (platform == "darwin")
-    lib_path = path.join(cwd, "/bin/mac/libtdjson.1.7.3.dylib");
+    lib_path = path.join(cwd, "/bin/mac/libtdjson.dylib");
   else if (platform == "linux")
-    lib_path = path.join(cwd, "/bin/linux/libtdjson.so.1.7.3");
-
-  // if (fs.existsSync(lib_path)) {
-  console.log("return path:", lib_path);
+    lib_path = path.join(cwd, "/bin/linux/libtdjson.so");
 
   return lib_path
-  // }
-
-  // return null
 }
